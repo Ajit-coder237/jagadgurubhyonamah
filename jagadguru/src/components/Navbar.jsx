8118518c8d6be10fa10932ad141cc996b6a43adc
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-23 21:37:51 +0545

    jagadgurubhyo namah

--- jagadguru/src/components/Navbar.jsx
+++ jagadguru/src/components/Navbar.jsx
@@ -1,50 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navItems = [
+    { to: "/", text: "योगीराज स्वामीजी", hoverWidth: "w-40" },
+    { to: "/acharya", text: "आचार्य परम्परा", hoverWidth: "w-20" },
+    { to: "/some-other-page", text: "लीलामृतम", hoverWidth: "w-14" },
+  ];
+
   return (
     <div>
       <nav className="bg-transparent mt-2 border-t-[0.01rem] border-b-[0.01rem] border-[#36454f]">
-        <ul className="flex p-4 gap-16 text-xl justify-start items-center">
-          <li className="font-laila font-bold text-[#556b2f]">
-            <Link to="/">
-              <div className="flex flex-col group hover:text-[#3e2a47]">
-                <div className="flex justify-center items-center gap-12">
-                  <span>श्रीआद्यजगद्गुरु कमलनयनाचार्य</span>
-                  <div class="border-l-2 border-red-500 h-10"></div>
-                </div>
+        {/* Mobile Menu Button */}
+        <div className="md:hidden p-4 flex justify-end">
+          <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="text-[#556b2f] hover:text-[#3e2a47]"
+          >
+            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
 
-                <hr className="w-10 h-[0.1rem] border-t-2 border-orange-400 group-hover:w-40 transition-all duration-300 " />
-              </div>
-            </Link>
-          </li>
-          <li className="font-laila font-bold text-[#556b2f]">
-            <Link to="/acharya">
-              <div className="flex flex-col group hover:text-[#3e2a47]">
-                <div className="flex items-center gap-12">
-                  <span>आचार्य परम्परा</span>
-                  <div class="border-l-2 border-red-500 h-10"></div>
+        {/* Navigation Items */}
+        <ul
+          className={`
+            flex flex-col md:flex-row
+            p-4 md:gap-16 gap-8
+            text-xl
+            ${isMenuOpen ? "flex" : "hidden md:flex"}
+            md:justify-start
+            items-center
+          `}
+        >
+          {navItems.map((item, index) => (
+            <li
+              key={index}
+              className="font-laila font-bold text-[#556b2f] w-full md:w-auto"
+            >
+              <Link to={item.to} onClick={() => setIsMenuOpen(false)}>
+                <div className="flex flex-col group hover:text-[#3e2a47]">
+                  <div className="flex justify-center md:justify-start items-center gap-6 md:gap-12">
+                    <span className="text-center md:text-left">
+                      {item.text}
+                    </span>
+                    <div className="border-l-2 border-red-500 h-10 hidden md:block"></div>
+                  </div>
+                  <div className="flex justify-center md:justify-start">
+                    <hr
+                      className={`w-8 h-[0.1rem] border-t-2 border-orange-400 group-hover:${item.hoverWidth} transition-all duration-300`}
+                    />
+                  </div>
                 </div>
-                <div className=" ">
-                  <hr className="w-8 h-[0.1rem] border-t-2 border-orange-400 group-hover:w-20 transition-all duration-300 " />
-                </div>
-              </div>
-            </Link>
-          </li>
-
-          <li className="font-laila font-bold text-[#556b2f]">
-            <Link to="/some-other-page">
-              <div className="flex flex-col group hover:text-[#3e2a47]">
-                <div className="flex justify-center items-center gap-12">
-                  <span>लीलामृतम</span>
-                  <div class="border-l-2 border-red-500 h-10"></div>
-                </div>
-                <div className=" ">
-                  <hr className="w-8 h-[0.1rem] border-t-2 border-orange-400 group-hover:w-14 transition-all duration-300 " />
-                </div>
-              </div>
-            </Link>
-          </li>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
